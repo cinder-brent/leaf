c3c117ea3c838c990d2a2bff00640cd60596132e
cinder-brent <100439491+cinder-brent@users.noreply.github.com>
2022-11-26 18:53:51 -0800

    Add files via upload

--- php/classes/Filter.php
+++ php/classes/Filter.php
@@ -8,18 +8,18 @@
  * @package Leaf
  */
 class Filter {
-	
+
 	/** Arguments {@see setValue} */
-		
+
 		/** @var array $prefixArgs Prefix arguments. */
 		public array $prefixArgs = [];
-		
+
 		/** @var array $suffixArgs Suffix arguments. */
 		public array $suffixArgs = [];
-		
+
 	/** @var array|string $filter A callable filter. */
 	private array|string $filter;
-	
+
 	/**
 	 * Constructs the filter by setting $this->args and $this->filter.
 	 *
@@ -31,24 +31,24 @@ class Filter {
 		array $prefixArgs = [],
 		array $suffixArgs = []
 	) {
-		
+
 		// Set
-			
+
 			// Filter
 			$this->set_filter($filter);
-			
+
 			// Arguments
-				
+
 				// Prefix arguments
 				$this->prefixArgs = $prefixArgs;
-				
+
 				// Suffix arguments
 				$this->suffixArgs = $suffixArgs;
-				
+
 	}
-	
+
 	// $filter
-		
+
 		/**
 		 * Set the filter.
 		 *
@@ -57,24 +57,24 @@ class Filter {
 		 * @return callable $filter
 		 */
 		function set_filter(callable $filter) {
-			
+
 			// Simply set $this->filter to $filter and return
 			return $this->filter = $filter;
-			
+
 		}
-		
+
 		/**
 		 * Get the filter.
 		 *
 		 * @return callable
 		 */
 		function get_filter() {
-			
+
 			// Simply return $this->filter
 			return $this->filter;
-			
+
 		}
-		
+
 	/**
 	 * Filter $input.
 	 *
@@ -83,19 +83,17 @@ class Filter {
 	 * @return mixed The filtered $input.
 	 */
 	function filter($input = 1) {
-		
+
 		// Filter and return $input
 		return call_user_func_array(
 			$this->filter,
-			[array_merge(
+			array_merge(
 				$this->prefixArgs,
-				$input,
+				[$input],
 				$this->suffixArgs
-			)]
+			)
 		);
-		
-	}
-	
-}
 
-?>
+	}
+
+}
--- php/classes/Leaf.php
+++ php/classes/Leaf.php
@@ -4,75 +4,61 @@
 require_once CLASS_DIR . "Filters.php";
 
 /**
- * @todo Finish comments...
+ * Leaf is a teeny, tiny, universal class for PHP
+ * 
+ * Scale huge systems, do everything you ever wanted to do in a computer program
+ * with Leaf. The justification? It's actually shorter to use Leaf than to use a
+ * traditional database like MariaDB.
+ * 
+ * **See the code for all the details, it's well commented!**
  *
  * @author Brent Bevans <cinder.brent@gmail.com>
  * @package Leaf
  * @version 0.0.1
  */
 class Leaf {
-	
+
 	/** @var mixed $value The value of this Leaf. {@see setValue} */
 	private $value;
-	
+
 	/**
 	 * Assigns $Filters and $value
 	 *
 	 * @param string $dataPath Location of the file to store data in.
-	 * @param Filters $oFilters Filters to filter output with. {@see getValue}
 	 * @param Filters $iFilters Filters to filter input with. {@see setValue}
+	 * @param Filters $oFilters Filters to filter output with. {@see getValue}
 	 */
 	function __construct(
 		public string $dataPath = "",
-		public Filters $oFilters = new Filters,
-		public Filters $iFilters = new Filters
+		public Filters $iFilters = new Filters,
+		public Filters $oFilters = new Filters
 	) {
-		
+
 		// If we have a $dataPath
-		if ($dataPath) {
-			
-			// If $dataPath is_file, is_readable, and is_writable
-			if (
-				is_file($dataPath) &&
-				is_readable($dataPath) &&
-				is_writable($dataPath)
-			)
-				
-				// Read $value
-				$this->value = file_get_contents($dataPath);
-				
-			// Otherwise
-			else
-				
-				/** @todo Finish... */
-				die("Leaf::__construct - \$dataPath either isn't a file, isn't\
-					readable, or isn't writable.");
-				
-			// Load it
-			$this->value = file_get_contents($
-			
-		}
-		
-		// Otherwise
-		else
-			
-			// die
-			die("You must supply a data path!");
-			
+		if (
+			$dataPath &&
+			is_file($dataPath) &&
+			is_readable($dataPath) &&
+			is_writable($dataPath)
+		)
+
+			// Read $value
+			$this->value = file_get_contents($dataPath);
+
 	}
-	
+
 	/**
 	 * Filters $value with $oFilters->filter() and returns it
 	 *
 	 * @return mixed Filtered $value.
 	 */
 	function getValue() {
-		
+
 		// Return filtered input
 		return $this->oFilters->filter($this->value);
-		
+
 	}
-	
+
 	/**
 	 * Filter and set $value
 	 *
@@ -86,21 +72,19 @@ class Leaf {
 	 * @return mixed The filtered value.
 	 */
 	function setValue($value = null) {
-		
+
 		// Filter $value
 		$this->value = $this->iFilters->filter($value);
-		
+
 		// Save it to disk
 		file_put_contents(
 			$this->dataPath,
 			$this->value
 		);
-		
+
 		// And return it
 		return $this->value;
-		
-	}
-	
-}
 
-?>
+	}
+
+}
